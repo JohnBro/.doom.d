889750a276c5d9c2e5d2aebf8b36da5837489d09
Junbo <junblu@qti.qualcomm.com>
2022-03-25 21:01:14 +0800

    Hack for embark to open directoy with kbd 'X'

--- config.el
+++ config.el
@@ -118,6 +118,22 @@
   (setq org-use-sub-superscripts '{})
   (setq org-export-with-sub-superscripts '{}))
 
+(after! embark
+  (defun consult-directory-externally (file)
+    "Open FILE path externally using the default application of the system."
+    (interactive "fOpen externally: ")
+    (if (and (eq system-type 'windows-nt)
+             (fboundp 'w32-shell-execute))
+        (shell-command-to-string (encode-coding-string (replace-regexp-in-string "/" "\\\\"
+                                                                                 (format "explorer.exe %s" (file-name-directory (expand-file-name file)))) 'gbk))
+      (call-process (pcase system-type
+                      ('darwin "open")
+                      ('cygwin "cygstart")
+                      (_ "xdg-open"))
+                    nil 0 nil
+                    (file-name-directory (expand-file-name file)))))
+  (define-key embark-file-map (kbd "X") #'consult-directory-externally))
+
 (use-package! websocket
     :after org-roam)
 
