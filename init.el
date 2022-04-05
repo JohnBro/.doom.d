95afbcaac1e02c91b10dd82118693d2cf7445e79
Junbo Lu <johnbro@foxmail.com>
2022-04-05 23:54:03 +0800

    Fix for eaf

--- init.el
+++ init.el
@@ -51,7 +51,7 @@
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
-       unicode           ; extended unicode support for various languages
+       ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        (window-select     ; visually switch windows
@@ -123,7 +123,10 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       (cc +lsp)         ; C > C++ == 1
+       ;;(cc +lsp)         ; C > C++ == 1
+       (:if (not IS-WINDOWS)
+        (cc +lsp)
+        (cc))
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -148,10 +151,14 @@
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
-       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
+       (:if (not IS-WINDOWS)
+        (java +lsp)       ; the poster child for carpal tunnel syndrome
+        (java))
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
-       ;;kotlin            ; a better, slicker Java(Script)
+       (:if (not IS-WINDOWS)
+        (kotlin +lsp)      ; a better, slicker Java(Script)
+        (kotlin))
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
@@ -210,4 +217,6 @@
        ;;literate
        (default +bindings +smartparens)
        :private
-       qualcomm)
+       qualcomm
+       (:if (not IS-WINDOWS) eaf)
+       )
