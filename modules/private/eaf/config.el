d504760f95e258ed96b4101dc089257633114b13
Junbo Lu <johnbro@foxmail.com>
2022-04-05 23:45:44 +0800

    Add more modules

---
+++ modules/private/eaf/config.el
@@ -0,0 +1,14 @@
+;;; private/eaf/config.el -*- lexical-binding: t; -*-
+(use-package! eaf
+  :config
+  (require 'eaf-browser)
+  (require 'eaf-pdf-viewer)
+  (require 'eaf-video-player)
+  (require 'eaf-markdown-previewer)
+  (require 'eaf-org-previewer)
+  (require 'eaf-file-manager)
+  (require 'eaf-mindmap)
+  (require 'eaf-file-browser)
+  (require 'eaf-file-sender)
+  (require 'eaf-airshare)
+  )
